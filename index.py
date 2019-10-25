38dd5b0e88a7138227d142a08e2eed14b6fb1f24
Luis Chitala <lrchitala@gmail.com>
2019-10-25 18:30:04 -0500

    Database connection successfully done

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ from tkinter import *
 import sqlite3 
 
 class Product:
+    db_name = 'database.db'
+
     def __init__(self,window):
         self.wind = window 
         self.wind.title('Product Application')
@@ -30,6 +32,28 @@ class Product:
         self.tree.grid(row = 4, column = 0, columnspan = 2)
         self.tree.heading('#0', text = 'name', anchor = CENTER)
         self.tree.heading('#1', text = 'price', anchor = CENTER)
+
+        self.get_products()
+    
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+            cursor = conn.cursor()
+            result = cursor.execute(query,parameters)
+            conn.commit()
+        return result
+    
+    def get_products(self):
+        #cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        #quering data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        #filling data
+        for row in db_rows:
+            self.tree.insert('', 0, text = row[1],values = row[2])
+           
 if __name__ == '__main__':
     window = Tk()
     application = Product(window)
