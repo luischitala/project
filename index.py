26fd48cbf8e49e891cbbe21f9036ecd1f71dd06f
Luis Chitala <lrchitala@gmail.com>
2019-10-25 17:59:45 -0500

    Button and table created

---
+++ index.py
@@ -0,0 +1,36 @@
+from tkinter import ttk 
+from tkinter import *
+
+import sqlite3 
+
+class Product:
+    def __init__(self,window):
+        self.wind = window 
+        self.wind.title('Product Application')
+
+        #creating a frame containter
+        frame = LabelFrame(self.wind, text = 'Register a new prduct')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+        #name input
+        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
+
+        #price input
+        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+        #button and product
+        ttk.Button(frame, text = 'Save Product').grid(row = 3, columnspan = 2, sticky = W + E)
+
+        #table
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'name', anchor = CENTER)
+        self.tree.heading('#1', text = 'price', anchor = CENTER)
+if __name__ == '__main__':
+    window = Tk()
+    application = Product(window)
+    window.mainloop()
